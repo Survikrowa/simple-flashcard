3b4edc1e525eef30d4a8da736f74d27de446375d
survikrowa <szymonhrabia60@gmail.com>
2021-02-02 19:39:08 +0100

    init react router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Main } from "./pages/main/Main";
+import { NewSet } from "./pages/newSet/NewFlashCardKit";
 import styles from "./App.module.scss";
 
 export function App() {
-  return <div>init</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/newset">
+          <NewSet />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
