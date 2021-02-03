602261330ee9b3eb50c4ea7dd55c80d1bd41ef56
survikrowa <szymonhrabia60@gmail.com>
2021-02-03 13:32:53 +0100

    Add disabled prop for Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -6,6 +6,7 @@ type ButtonProps = {
   type?: "button" | "submit";
   role?: "primary" | "danger";
   onClick?: () => void;
+  disabled?: boolean;
 };
 
 export const Button = ({
@@ -13,12 +14,14 @@ export const Button = ({
   type = "button",
   role = "primary",
   onClick = () => {},
+  disabled = false,
 }: ButtonProps) => {
   return (
     <button
       type={type}
       className={clsx(styles.button, styles[role])}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
