50bbad523d479d8244aa750f7700d0ec0ff21fa9
survikrowa <szymonhrabia60@gmail.com>
2021-02-03 20:15:08 +0100

    Add dummy data for flashcards

---
+++ src/flashcardsData/flashcardsData.ts
@@ -0,0 +1,112 @@
+export const flashcardsData = [
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+  {
+    title: "Matma",
+    description: "Wzory matematyczne",
+    flashcards: [
+      {
+        term: "Wzór skróconego mnożenia",
+        description: "Jest to wzór skróconego mnożenia",
+      },
+    ],
+  },
+];
